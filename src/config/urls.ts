be43a8eb97f2ded060fcf31bb6d4ec4940bc14d8
wesloong <wesloong@gmail.com>
2026-05-21 17:55:38 +0800

    修改邀请链接

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -1,3 +1,3 @@
 /** 开户与交易平台（客户端门户） */
 export const TRADING_PLATFORM_URL =
-  process.env.NEXT_PUBLIC_TRADING_PLATFORM_URL ?? "https://hung3svgo.plusiaa.com";
+  process.env.NEXT_PUBLIC_TRADING_PLATFORM_URL ?? "https://qudbwiz7a.plusiaa.com";
